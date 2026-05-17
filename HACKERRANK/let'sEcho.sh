5346c76863cb2afacc673787a462a1b792758796
Rajan Poudel <raajan.works@gmail.com>
2026-05-17 10:49:37 +0545

    Add files via upload

---
+++ HACKERRANK/let'sEcho.sh
@@ -0,0 +1,19 @@
+# Write a bash script that prints the string "HELLO".
+
+# Input Format
+# There is no input file required for this problem.
+
+# Output Format
+# HELLO
+
+# Sample Input
+# -
+
+# Sample Output
+# HELLO
+
+# Explanation
+# -
+
+# Link: https://www.hackerrank.com/challenges/bash-tutorials-lets-echo/
+echo "HELLO";
